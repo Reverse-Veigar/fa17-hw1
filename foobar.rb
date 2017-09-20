dd9e8a7dbfcc7bbc9381d79cd3939a2f140894b1
Daniel Vazquez <dvazquez21@berkeley.edu>
2017-09-20 00:17:06 -0700

    All tests work, Aid from StackOverflow

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,14 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    # How does map differ from map! ? Is it space allocation?
+    a.map! { |c| c.to_i + 2 }
+    #StackOverflow suggestion to get even enteries
+    a.values_at(* a.each_index.select {|i| i.even?})
+    #get rid of dups
+    a.uniq!
+    # what is the difference between reduce and inject?
+    [a.reduce(:+),10].min
   end
 end
 
